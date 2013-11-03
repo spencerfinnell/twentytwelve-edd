6cc92729496d9d385eeb648a84689b166af30fce
Spencer Finnell <spencerfinnell@gmail.com>
2013-11-03 17:04:43 -0500

    Respect button color, and translate string.

--- edd_templates/shortcode-content-image.php
+++ edd_templates/shortcode-content-image.php
@@ -5,12 +5,16 @@
  * @package Twenty_Twelve_EDD
  * @since Twenty Twelve EDD 1.0
  */
+
+global $edd_options;
+
+$color = isset( $edd_options[ 'checkout_color' ] ) ? $edd_options[ 'checkout_color' ] : 'blue';
 ?>
 
 <a href="<?php the_permalink(); ?>" rel="bookmark" class="item-archive-preview">
 	<span class="overlay"><span class="overlay-inner">
 		<strong><?php the_title(); ?></strong>
-		<span class="button edd-submit blue">View Details</span>
+		<span class="button edd-submit <?php echo $color; ?>"><?php _e( 'View Details', 'twentytwelve-edd' ); ?></span>
 	</span></span>
 
 	<?php the_post_thumbnail( 'large' ); ?>
