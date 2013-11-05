88db1f713975444274f0c753e836042a98bed6ef
Spencer Finnell <spencerfinnell@gmail.com>
2013-11-04 19:51:41 -0500

    Identity crisis.

--- single-download.php
+++ single-download.php
@@ -23,9 +23,9 @@ get_header(); ?>
 					<div class="item-prices">
 						<h1 class="entry-title"><?php the_title(); ?></h1>
 
-						<?php echo edd_get_purchase_link( apply_filters( 'marketstrap_edd_get_purchase_link', array() ) ); ?>
+						<?php echo edd_get_purchase_link( apply_filters( 'twentytwelve_edd_get_purchase_link', array() ) ); ?>
 
-						<?php echo get_the_term_list( $post->ID, 'download_tag', '<p class="tag-links"><span class="screen-reader-text">' . __( 'Tagged with:', 'photo-one' ) . '</span>', ',&nbsp;', '</p>' ); ?>
+						<?php echo get_the_term_list( $post->ID, 'download_tag', '<p class="tag-links"><span class="screen-reader-text">' . __( 'Tagged with:', 'twenty-twelve-edd' ) . '</span>', ',&nbsp;', '</p>' ); ?>
 					</div>
 				</div>
 
@@ -34,9 +34,9 @@ get_header(); ?>
 				<?php get_template_part( 'content', get_post_format() ); ?>
 
 				<nav class="nav-single">
-					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
-					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
+					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve-edd' ); ?></h3>
+					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve-edd' ) . '</span> %title' ); ?></span>
+					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve-edd' ) . '</span>' ); ?></span>
 				</nav><!-- .nav-single -->
 
 				<?php comments_template( '', true ); ?>
